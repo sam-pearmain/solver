62f0a1849f847dc5016cfed16ba3f0629df621c6
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-03-07 18:07:35 +0000

    added 2d grid stuff

--- src/grid/grid.rs
+++ src/grid/grid.rs
@@ -7,19 +7,14 @@ use super::boundary::{Boundary, BoundaryType};
 #[derive(Debug)]
 pub struct Vertex {
     i: usize, 
-    j: usize, 
-    k: usize, 
+    j: usize,
     x: f64,
     y: f64,
-    z: f64,
 }
 
 impl Vertex {
-    pub fn new(
-        i: usize, j: usize, k: usize,
-        x: f64  , y: f64,   z: f64,
-    ) -> Self {
-        Vertex { i, j, k, x, y, z }
+    pub fn new(i: usize, j: usize, x: f64  , y: f64) -> Self {
+        Vertex { i, j, x, y }
     }
 }
 
@@ -27,56 +22,65 @@ impl Vertex {
 pub struct Block {
     nx: usize,
     ny: usize,
-    nz: usize,
     vertices: Vec<Vertex>
 }
 
 impl Block {
-    fn new() -> Self {
-        Block { 
-            nx: 0, 
-            ny: 0, 
-            nz: 0, 
-            vertices: Vec::new(),        
+    pub fn create_with_dimensions(nx: usize, ny: usize) -> Self {
+        let mut vertices: Vec<Vertex> = Vec::with_capacity(nx * ny);
+        for i in 0..nx {
+            for j in 0..ny {
+                vertices.push(Vertex::new(i, j, 0.0, 0.0));
+            }
         }
+        Block { nx, ny, vertices }
     }
-
-    pub fn with_dimensions(mut self, nx: usize, ny: usize, nz: usize) -> Self {
-        self.nx = nx;
-        self.ny = ny;
-        self.nz = nz;
-        self
-    }
-
-    pub fn initialise_vertices()
 
     pub fn from_plot3d(p3d_filepath: &str) -> Result<Self, &'static str> {
-        let blocks = read_plot3d_ascii(p3d_filepath)
-            .expect("cannot read p3d file");
+        let blocks = read_plot3d_ascii(p3d_filepath).expect("cannot read p3d file");
         if blocks.len() > 1 {
-            return Err("only single block meshes are currently supported");
+            eprintln!("warning: plot3d file contains multiple blocks. only date from the first block will be used");
         }
-        let p3d_block = blocks.first().unwrap();
-        let mut block = Block::new()
-            .with_dimensions(
-                p3d_block.imax as usize, 
-                p3d_block.jmax as usize, 
-                p3d_block.kmax as usize, 
-            );
-        block.set_x_coordinates(p3d_block.x.clone())?;
-        block.set_y_coordinates(p3d_block.y.clone())?;
-        block.set_z_coordinates(p3d_block.z.clone())?;
-        Ok(block)
+        let block = blocks.first().expect("cannot read the first block");
+
+        let total_vertices = block.x.len();
+        let mut vertices: Vec<Vertex> = Vec::with_capacity(total_vertices);
+        let nx = block.imax as usize;
+        let ny = block.jmax as usize;
+
+        for j in 0..ny {
+            for i in 0..nx {
+                let index = i + (j * nx);
+                let vertex = Vertex::new(i, j, block.x[index], block.y[index]);
+                vertices.push(vertex);
+            }
+        }
+
+        Ok(Block { nx, ny, vertices })
     }
 
     pub fn total_grid_points(&self) -> usize {
-        self.nx * self.ny * self.nz
+        self.nx * self.ny
+    }
+
+    pub fn export_txt(&self, filename: &str) -> std::io::Result<()> {
+        use std::fs::File;
+        use std::io::Write;
+
+        let mut file = File::create(filename)?;
+
+        writeln!(file, "block dimensions: {} x {}", self.nx, self.ny)?;
+        for vertex in &self.vertices {
+            writeln!(file, "{} {}, ({:.3}, {:.3})", vertex.i, vertex.j, vertex.x, vertex.y)?;
+        }
+        
+        Ok(())
     }
 }
 
 impl std::fmt::Display for Block {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "block dimensions ({}, {}, {})", self.nx, self.ny, self.nz)
+        write!(f, "block dimensions ({}, {})", self.nx, self.ny)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use grid::grid::Block;
 fn main() -> Result<(), &'static str> {
     let block = Block::from_plot3d("double-wedge-fine.p3d")
         .map_err(|_| "could not create block")?;
-    println!("{}", block);
+    block.export_txt("block.txt")
+        .map_err(|_| "what")?;
     Ok(())
 }
