83efb46369287cd8d2cc1c94f473d48fd814b838
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-03-11 14:35:12 +0000

    changing lots of stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 mod meshing;
 
-use meshing::mesh::Mesh;
-
 fn main() -> Result<(), &'static str> {
-    let mut mesh = Mesh::from_vtk("structured-double-wedge.vtk")?;
-    mesh.build_elements()?;
-    mesh.plot_dbg()?;
-    Ok(())
+    // let mut mesh = Mesh::from_vtk("structured-double-wedge.vtk")?;
+    // mesh.build_elements()?;
+    // mesh.plot_dbg()?;
+    // Ok(())
+    todo!()
 }
--- src/meshing/nodes.rs
+++ src/meshing/nodes.rs
@@ -21,11 +21,35 @@ pub struct Node3D {
     pub z:  f64,
 }
 
-pub trait Node { const DIMENSIONS: usize; }
+pub trait Node { 
+    const DIMENSIONS: usize;
 
-impl Node for Node1D { const DIMENSIONS: usize = 1; }
-impl Node for Node2D { const DIMENSIONS: usize = 2; }
-impl Node for Node3D { const DIMENSIONS: usize = 3; }
+    fn set_id(&mut self, id: usize); 
+}
+
+impl Node for Node1D { 
+    const DIMENSIONS: usize = 1;
+
+    fn set_id(&mut self, id: usize) {
+        self.id = id
+    } 
+}
+
+impl Node for Node2D { 
+    const DIMENSIONS: usize = 2; 
+
+    fn set_id(&mut self, id: usize) {
+        self.id = id
+    }
+}
+
+impl Node for Node3D { 
+    const DIMENSIONS: usize = 3;
+
+    fn set_id(&mut self, id: usize) {
+        self.id = id
+    } 
+}
 
 impl Node1D {
     pub fn new(id: usize, x: f64) -> Self {
@@ -52,5 +76,47 @@ pub trait Dimensioned {
 impl<T: Node> Dimensioned for T {
     fn dimensions() -> usize {
         Self::DIMENSIONS
+    }
+}
+
+#[derive(Debug)]
+pub struct NodeCollection<T: Node> {
+    nodes: Vec<T>,
+}
+
+impl<T: Node> NodeCollection<T> {
+    fn new() -> Self {
+        NodeCollection { nodes: Vec::new() }
+    }
+
+    fn with_capacity(capacity: usize) -> Self {
+        NodeCollection { nodes: Vec::with_capacity(capacity) }
+    }
+
+    fn push_node(&mut self, n: T) {
+        self.nodes.push(n);
+    }
+
+    fn sanitise(&mut self) {
+        for (i, node) in self.nodes.iter_mut().enumerate() {
+            node.set_id(i);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_node_collection() {
+        let mut node_colletion: NodeCollection<Node1D> = NodeCollection::new();
+        for i in 0..10 {
+            let n = Node1D::new(i + 1, (i * 2) as f64);
+            node_colletion.push_node(n);
+        }
+        println!("{:?}", node_colletion);
+        node_colletion.sanitise();
+        println!("{:?}", node_colletion);
     }
 }
